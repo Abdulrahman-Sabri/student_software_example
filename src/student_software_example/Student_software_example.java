258192dec01861c751b8d999e62be196883c6881
Abdulrahman-Sabri <50593594+Abdulrahman-Sabri@users.noreply.github.com>
2019-10-31 16:08:40 -0400

    Update Student_software_example.java

--- src/student_software_example/Student_software_example.java
+++ src/student_software_example/Student_software_example.java
@@ -2,6 +2,7 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+ todays date is 2019/oct/31
  */
 package student_software_example;
 
